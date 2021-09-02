e7c5628861bb557035174a2475483237b8982172
avula-mounika <goudmounika13@gmail.com>
2021-09-02 19:26:23 +0530

    Part Time Employee Wage

--- EmpWage.java
+++ EmpWage.java
@@ -5,9 +5,13 @@ public static void main(String[] args) {
 	int EMP_RATE_PER_HOUR = 20;
 	int empHrs = 0;
 	int empWage = 0;
+	int PART_TIME =2;
+	int FULL_TIME =1;
 	double empcheck = Math.floor(Math.random() *10) % 2;
-	if (empcheck == 1)
+	if (empcheck == FULL_TIME)
 		empHrs = 8;
+	else if (empcheck == PART_TIME)
+		empHrs =4 ;
 	else 
 		empHrs =0;
 	empWage = empHrs * EMP_RATE_PER_HOUR;
