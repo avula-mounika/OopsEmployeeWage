76cfedf8a39e9cada8675f5f347665fa262bf775
avula-mounika <goudmounika13@gmail.com>
2021-09-02 19:20:00 +0530

    Daily Employee Wage

--- EmpWage.java
+++ EmpWage.java
@@ -2,12 +2,15 @@
 public class EmpWage {
 
 public static void main(String[] args) {
-	System.out.println("welcome to employee wage computation program");
+	int EMP_RATE_PER_HOUR = 20;
+	int empHrs = 0;
+	int empWage = 0;
 	double empcheck = Math.floor(Math.random() *10) % 2;
-	//computation
-	if  (empcheck == 1)
-		System.out.println("Employee is Present: " + empcheck);
-	else
-		System.out.println("Employee is Absent: " +empcheck);
+	if (empcheck == 1)
+		empHrs = 8;
+	else 
+		empHrs =0;
+	empWage = empHrs * EMP_RATE_PER_HOUR;
+	System.out.println("Emp Wage: " + empWage);
 	}
 }
