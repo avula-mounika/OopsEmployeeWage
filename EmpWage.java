38680752de77f87d0c20162504b5e4c56c5f7002
avula-mounika <goudmounika13@gmail.com>
2021-09-02 19:57:06 +0530

    Compute emp wage

--- EmpWage.java
+++ EmpWage.java
@@ -6,8 +6,10 @@ public class EmpWage {
 	public static final int NUM_OF_WORKING_DAYS = 2;
 	public static final int MAX_HRS_IN_MONTH = 10;
 	
-	public static void main(String[] args) {
+	public static int computeEmpWage() {
+		//variables
 		int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+		//computation
 		while (totalEmpHrs <= MAX_HRS_IN_MONTH && 
 			   totalWorkingDays < NUM_OF_WORKING_DAYS) {
 			totalWorkingDays++;
@@ -27,5 +29,9 @@ public class EmpWage {
    	}
 	int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
 	System.out.println("Total Emp Wage: " + totalEmpWage);
+	return totalEmpWage;
+	}
+	public static void main(String[] args) {
+		computeEmpWage();
 	}
 }
