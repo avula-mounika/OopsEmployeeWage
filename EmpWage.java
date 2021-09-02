e47390306d3cb2e68dce8332d4970e881077d43e
avula-mounika <goudmounika13@gmail.com>
2021-09-02 19:31:17 +0530

    switch case

--- EmpWage.java
+++ EmpWage.java
@@ -1,20 +1,27 @@
 
 public class EmpWage {
+	public static final int IS_PART_TIME = 1;
+	public static final int IS_FULL_TIME = 2;
+	public static final int EMP_RATE_PER_HOUR = 20;
 
-public static void main(String[] args) {
+	public static void main(String[] args) {
 	int EMP_RATE_PER_HOUR = 20;
 	int empHrs = 0;
 	int empWage = 0;
-	int PART_TIME =2;
-	int FULL_TIME =1;
-	double empcheck = Math.floor(Math.random() *10) % 2;
-	if (empcheck == FULL_TIME)
+	
+	int empcheck = (int) Math.floor(Math.random() *10) % 3;
+	switch (empcheck) {
+	case IS_PART_TIME:
+		empHrs = 4;
+		break;
+	case IS_FULL_TIME:
 		empHrs = 8;
-	else if (empcheck == PART_TIME)
-		empHrs =4 ;
-	else 
-		empHrs =0;
+		break;
+	default:
+		empHrs = 0;
+	}
 	empWage = empHrs * EMP_RATE_PER_HOUR;
 	System.out.println("Emp Wage: " + empWage);
+	
 	}
 }
