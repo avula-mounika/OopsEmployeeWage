6ccd2db146cba6c08a39e2450d5250452d735d7a
avula-mounika <goudmounika13@gmail.com>
2021-09-02 18:59:10 +0530

    attendance

--- EmpWage.java
+++ EmpWage.java
@@ -3,6 +3,11 @@ public class EmpWage {
 
 public static void main(String[] args) {
 	System.out.println("welcome to employee wage computation program");
-
+	double empcheck = Math.floor(Math.random() *10) % 2;
+	//computation
+	if  (empcheck == 1)
+		System.out.println("Employee is Present: " + empcheck);
+	else
+		System.out.println("Employee is Absent: " +empcheck);
 	}
 }
